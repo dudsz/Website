852a67865cce5fb79deb15e0884d28947510c480
dudsz <markus.erlach@gmail.com>
2016-05-18 22:30:11 +0200

    Added some js

---
+++ JavaScript/signup.js
@@ -0,0 +1,6 @@
+
+function setTags(username, email, would) {
+	document.getElementById("lblUsername").innerHTML = username;
+	document.getElementById("lblEmail").innerHTML = email;
+	document.getElementById("lblWould").innerHTML = would;
+}
